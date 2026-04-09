e6f9e38876321cee9b2bab8e8c03ea1419b0c611
LaurentLienhard <laurent.lienhard@outook.com>
2026-04-09 15:40:16 +0200

    push test

--- test.ps1
+++ test.ps1
@@ -2,13 +2,9 @@
 Get-PSATDhcpScopeInfo -ComputerName dsddhcp02 -Credential (Get-Secret AdmAccount)
 
 
-Building Module to C:\Users\llienhard\Documents\01-DEV\Github\PSAdminTools\output\module\PSAdminTools...
-ERROR: An item with the same key has already been added. Key: PSATDhcpScope
-At C:\Users\llienhard\Documents\01-DEV\Github\PSAdminTools\output\RequiredModules\ModuleBuilder\3.1.8\ModuleBuilder.psm1:1130 char:17
-+                 Get-Module $OutputManifest -ListAvailable
-+                 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-At C:\Users\llienhard\Documents\01-DEV\Github\PSAdminTools\output\RequiredModules\Sampler\0.119.1\tasks\Build-Module.ModuleBuilder.build.ps1:41 char:1
-+ Task Build_ModuleOutput_ModuleBuilder {
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Build FAILED. 4 tasks, 1 errors, 0 warnings 00:00:12.0191111
-Get-Module: An item with the same key has already been added. Key: PSATDhcpScope
+Get-PSATDhcpScopeInfo: C:\Users\llienhard\Documents\01-DEV\Github\PSAdminTools\test.ps1:2:1
+Line |
+   2 |  Get-PSATDhcpScopeInfo -ComputerName dsddhcp02 -Credential (Get-Secret …
+     |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     | Failed to query DHCP server 'dsddhcp02': The term 'if' is not recognized as the name of a cmdlet, function, script file, or operable program. Check the spelling of the name, or if a
+     | path was included, verify that the path is correct and try again.
